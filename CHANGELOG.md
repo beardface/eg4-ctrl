8707a7932e3cb10b03ac3ab28a685201844140df
Justin Hawkins <jhawkins@riotgames.com>
2026-03-08 10:15:28 -0500

    chore: bump version to 0.1.3 and fix workflow

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,11 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.1.3] - 2026-03-08
+
+### Fixed
+- GitHub Actions workflow release tag.
+
 ## [0.1.2] - 2026-03-08
 
 ### Changed
--- eg4_monitor/__init__.py
+++ eg4_monitor/__init__.py
@@ -2,7 +2,7 @@ from .client import EG4Client, AsyncEG4Client
 from .models import InverterData, BatteryInfo
 from .exceptions import EG4Error, AuthError, APIError, SessionError
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __all__ = [
     "EG4Client",
     "AsyncEG4Client",
